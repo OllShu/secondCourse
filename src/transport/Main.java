ee0b71dca4bcd0e4b3091a888c86f031b417c4eb
dns <shulgaol@mail.ru>
2023-02-16 13:25:30 +0200

    домашка урока Введение в коллекции. Мапы.

--- src/transport/Main.java
+++ src/transport/Main.java
@@ -1,7 +1,9 @@
 package transport;
 import transport.Transport.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,56 +28,37 @@ public class Main {
 
         Mechanic mechanic1 = new Mechanic("Дмитрий Волков", "Альфа");
         Mechanic mechanic2 = new Mechanic("Степан Мосин", "Зет");
-        Mechanic mechanic3 = new Mechanic("Александр Яковлев", "Лучшая");
+        Mechanic mechanic3 = new Mechanic("Александр Яковлев", "Омега");
 
 
-
-        Transport[] transports = new Transport[12];
-        transports[0] = new Car("Lada", "Granta", 1.6, driverWithLicenseBS[0],
+        Transport car1 = new Car("Lada", "Granta", 1.6, driverWithLicenseBS[0],
                 List.of(mechanic1, mechanic2), CarBodyType.sedan);
-        transports[1] = new Car("Audi", "A8 50 L TDI quattro", 3.0, driverWithLicenseBS[1],
+        Transport car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, driverWithLicenseBS[1],
                 List.of(mechanic1, mechanic3), CarBodyType.sedan);
-        transports[2] = new Car("BMW", "Z8", 3.0, driverWithLicenseBS[2],
+        Transport car3 = new Car("BMW", "Z8", 3.0, driverWithLicenseBS[2],
                 List.of(mechanic2, mechanic3), CarBodyType.sedan);
-        transports[3] = new Car("Kia", "Sportage 4-го поколения", 2.0, driverWithLicenseBS[3],
+        Transport car4 = new Car("Kia", "Sportage 4-го поколения", 2.0, driverWithLicenseBS[3],
                 List.of(mechanic1, mechanic2, mechanic3), CarBodyType.crossover);
-
-     /*   for (Car car : cars) {
-            System.out.println(cars);
-            outputInfo(car.getT().getFIO(), car.getBrand());
-            car.passDiagnostics();
-        }
-
-      */
-
-       // Bus[] buses = new Bus[4];
-
-        transports[4] = new Bus("ПАЗ", "4234", 4.4, driverWithLicenseDS[0],
+        Transport bus1 = new Bus("ПАЗ", "4234", 4.4, driverWithLicenseDS[0],
                 List.of(mechanic1, mechanic3), BusCapacity.extraSmall);
-        transports[5] = new Bus("Neoplan", "Skyliner", 10.5, driverWithLicenseDS[1],
+        Transport bus2 = new Bus("Neoplan", "Skyliner", 10.5, driverWithLicenseDS[1],
                 List.of(mechanic1, mechanic2), BusCapacity.extraLarge);
-        transports[6] = new Bus("Volvo", "B10M", 14.9, driverWithLicenseDS[2],
+        Transport bus3 = new Bus("Volvo", "B10M", 14.9, driverWithLicenseDS[2],
                 List.of(mechanic3), BusCapacity.large);
-        transports[7] = new Bus("Mercedes-Benz", "O405G", 11.9, driverWithLicenseDS[3],
+        Transport bus4 = new Bus("Mercedes-Benz", "O405G", 11.9, driverWithLicenseDS[3],
                 List.of(mechanic1, mechanic2, mechanic3), BusCapacity.large);
-
-       /* for (Bus bus : buses) {
-            System.out.println(bus);
-            outputInfo(bus.getT().getFIO(), bus.getBrand());
-            bus.passDiagnostics();
-        }
-
-        */
-
-        //Truck[] trucks = new Truck[4];
-        transports[8] = new Truck("КаМАЗ", "5490-S5", 12.0, driverWithLicenseCS[0],
+        Transport truck1 = new Truck("КаМАЗ", "5490-S5", 12.0, driverWithLicenseCS[0],
                 List.of(mechanic1), TruckCapacity.N1);
-        transports[9] = new Truck("Scania", "DC16", 15.6, driverWithLicenseCS[1],
+        Transport truck2 = new Truck("Scania", "DC16", 15.6, driverWithLicenseCS[1],
                 List.of(mechanic2, mechanic3), TruckCapacity.N2);
-        transports[10] = new Truck("MAN", "TGX", 12.4, driverWithLicenseCS[2],
+        Transport truck3 = new Truck("MAN", "TGX", 12.4, driverWithLicenseCS[2],
                 List.of(mechanic2), TruckCapacity.N2);
-        transports[11] = new Truck("Volvo", "D8K", 7.7, driverWithLicenseCS[3],
+        Transport truck4 = new Truck("Volvo", "D8K", 7.7, driverWithLicenseCS[3],
                 List.of(mechanic3), TruckCapacity.N3);
+
+        Transport[] transports = new  Transport[] {car1, car2, car3, car4, bus1, bus2, bus3, bus4, truck1, truck2, truck3, truck4};
+
+    //    Transport transport = transports[0];
 
         ArrayList<Transport> listRacer = new ArrayList<>();
         for (int i = 0; i < transports.length; i++) {
@@ -83,16 +66,28 @@ public class Main {
         }
 
         for (Transport transport : transports) {
-            System.out.println(transport);
+    //        System.out.println(transport);
             outputInfo(transport.getT().getFIO(), transport.getBrand(), transport.getModel());
             transport.passDiagnostics();
-    //        mechanic1.fixTheCar(transport);
-    //        mechanic1.performMaintenance(transport);
-            transport.findDriverAndMechanics(transport);
-
-        //    ServiceStation.addToQueue
-        //    ServiceStation.makeTechnicalInspection
+    //        transport.findDriverAndMechanics(transport);
         }
+
+
+        Map <Transport, List<Mechanic>> hashMap = new HashMap<>();
+        hashMap.put(car1, car1.getMechanicList());
+        hashMap.put(car2, car2.getMechanicList());
+        hashMap.put(car3, car3.getMechanicList());
+        hashMap.put(car4, car4.getMechanicList());
+        hashMap.put(bus1, bus1.getMechanicList());
+        hashMap.put(bus2, bus2.getMechanicList());
+        hashMap.put(bus3, bus3.getMechanicList());
+        hashMap.put(bus4, bus4.getMechanicList());
+        hashMap.put(truck1, truck1.getMechanicList());
+        hashMap.put(truck2, truck2.getMechanicList());
+        hashMap.put(truck3, truck3.getMechanicList());
+        hashMap.put(truck4, truck4.getMechanicList());
+
+        System.out.println(hashMap);
     }
 
     public static void outputInfo(String name, String brand, String model) {
